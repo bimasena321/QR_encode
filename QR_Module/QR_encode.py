76e48c0dbf732260418e8180443dcb79dd94cd6a
bima020 <64942799+bimasena321@users.noreply.github.com>
2022-11-06 18:21:00 +0700

    Update QR_encode.py

--- QR_Module/QR_encode.py
+++ QR_Module/QR_encode.py
@@ -3,7 +3,13 @@ import pyqrcode as QR
 a.system("clear")
 #dibuat oleh bimasena321
 def en_QR(a):
-  print("tulis terserah km!!")
+  print("""
+  =============================
+  = created by bimasena321    =
+  = hasil ada di folder pngQR =
+  = ketik terserah anda!!     =
+  =============================
+  """)
   b = input("buat barcode: ")
   kode = QR.create(b)
   kode.png("QR_kode.png",scale=10)
