c4cb160478a6c370b803c7fa075ed5a2f788eebf
bima020 <64942799+bimasena321@users.noreply.github.com>
2022-11-06 18:01:07 +0700

    Delete QR.py

--- QR.py
+++
@@ -1,7 +0,0 @@
-import pyqrcode as kodeQR
-import os as a
-a.system("clear")
-QR = kodeQR.create("pesan:namaku bima sena w.p")
-QR.png ("QRcodeQR.png",scale=7)
-a.system("mv QRcodeQR.png pngQR")
-print ("QR_code dibuat : sukses ")
