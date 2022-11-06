b3df26ff3563ba015cb7162b708a9ead96d30095
bima020 <64942799+bimasena321@users.noreply.github.com>
2022-11-06 14:36:33 +0700

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,6 +1,7 @@
 import pyqrcode as kodeQR
 import os as a
+a.system("clear")
 QR = kodeQR.create("pesan:namaku bima sena w.p")
 QR.png ("QRcodeQR.png",scale=7)
-a.system("mv QRcodeQR.png arduino_python")
+a.system("mv QRcodeQR.png pngQR")
 print ("QR_code dibuat : sukses ")
